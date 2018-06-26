0cf9b36c3ab9e8586a6f09300054f2b00b5e1938
Andriy Popov <33160603+AndriyPopovWeb@users.noreply.github.com>
2018-06-26 10:43:17 +0300

    add login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var User = require('../models/user');
+var User = require('../models/user.js');
+var jwt = require('jsonwebtoken');
+const config = require('../config/config.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,18 +15,43 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-    console.log(req.body.email);
-    if(req.body.email == ''  ) {
-        res.render('login', { title: 'Login', errormessage: 'error email or pass'});
+    if (req.body.email == '') {
+        res.render('login', { title: 'Login', errormessage: 'error email or pass' });
     }
-    if( req.body.password == '') {
-        res.render('login', { title: 'Login', errormessage: 'error email or pass'});
+    if (req.body.password == '') {
+        res.render('login', { title: 'Login', errormessage: 'error email or pass' });
     }
-    if(User.findOne({ email: req.body.email }, function (err, user) {
-        if(err) res.render('login', { title: 'Login', errormessage: 'error email or pass'});
-        if(user.password !== req.body.password) res.render('login', { title: 'Login', errormessage: 'error email or pass'});
-        return res.status(200).json({token: "123"});
-    }));
+    if (User.findOne({ 'email': req.body.email }, function(err, user) {
+            if (user == null) {
+                res.render('login', { title: 'Login', errormessage: 'error email or pass' });
+                return;
+            }
+            if (user.password != req.body.password) {
+                res.render('login', { title: 'Login', errormessage: 'error email or pass' });
+                return;
+            }
+            let token_user = {
+                name: user.name,
+                username: user.email,
+                admin: user.admin
+            };
+            let token = jwt.sign(token_user, config.secretWord, {
+                expiresIn: 60 * 10
+            });
+            if (user.admin) {
+                return res.json({
+                    status: 0,
+                    token: token,
+                    message: 'Admin',
+                });
+            } else {
+                return res.json({
+                    status: 1,
+                    token: token,
+                    message: 'NotAdmin',
+                });
+            }
+        }));
 });
 
 router.get('/register', function(req, res, next) {
@@ -40,7 +67,7 @@ router.post('/register', function(req, res, next) {
         admin: false
     });
     console.log(new_User);
-    new_User.save(function (err) {
+    new_User.save(function(err) {
         if (err) res.render('error', { message: 'write error' });
     });
     res.render('index', { title: 'Express' });
