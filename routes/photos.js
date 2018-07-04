0ff6c8466fd9e101ab0d48fafbcd819c7cd4e351
Andriy Popov <33160603+AndriyPopovWeb@users.noreply.github.com>
2018-07-04 09:52:52 +0300

    added grid fs

--- routes/photos.js
+++ routes/photos.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var User = require('../models/user.js');
-var jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user.js');
+const jwt = require('jsonwebtoken');
 const config = require('../config/config.js');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+
+const mongoURI = 'mongodb://127.0.0.1:27017/photos';
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
 
 router.get('/photos', function(req, res, next) {
     console.log("get_photos");
@@ -15,5 +57,10 @@ router.get('/photos', function(req, res, next) {
     ];
     res.json({ success: true, images: images });
 });
+
+router.post('/photos', upload.single('file'), (req, res) => {
+    // res.json({ file: req.file });
+    return res.json({success: true});
+  });
 
 module.exports = router;
