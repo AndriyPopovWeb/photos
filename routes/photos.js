603ecca75fc7ecad331cb0df6065372d41ede060
Andriy Popov <33160603+AndriyPopovWeb@users.noreply.github.com>
2018-07-05 16:21:44 +0300

    delete photos

--- routes/photos.js
+++ routes/photos.js
@@ -14,58 +14,57 @@ const methodOverride = require('method-override');
 
 const mongoURI = 'mongodb://127.0.0.1:27017/photos';
 
-// Create mongo connection
 const conn = mongoose.createConnection(mongoURI);
 
-// Init gfs
 let gfs;
 
 conn.once('open', () => {
-  // Init stream
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
 });
 
-// Create storage engine
 const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        // console.log(file);
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
 });
 const upload = multer({ storage });
 
 router.get('/photos', function(req, res, next) {
-  gfs.files.find().toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      return res.json({
-        err: 'No files exist'
-      });
-    }
-
-    // Files exist
-    return res.json(files);
-  });
+    gfs.files.find().toArray((err, files) => {
+        if (!files || files.length === 0) {
+            return res.json({
+                err: 'No files exist'
+            });
+        }
+        return res.json(files);
+    });
 });
 
 router.post('/photos', upload.single('file'), (req, res) => {
-    // res.json({ file: req.file });
-    
-    return res.json({success: true});
-  });
+    return res.json({ success: true });
+});
+
+router.delete('/photos/:id', (req, res) => {
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+        if (err) {
+            return res.status(404).json({ err: err });
+        }
+        return res.json({ success: true });
+    });
+});
 
 module.exports = router;
